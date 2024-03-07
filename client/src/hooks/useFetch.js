ad6eb8595dccb60e0c5e0928d73862da1f2621ae
Rounak Raman <raman.rounak@gmail.com>
2024-03-07 16:48:02 +0530

    Added sonsole.log(url)

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -15,6 +15,7 @@ const useFetch = (url) => {
           try{
             setLoading(true);
             const res = await axiosBaseReqAdd.get(url);   //ALTERNATIVE IF axioBaseReqAdd NOT MADE: const res = await axios.get(process.env.REACT_APP_API_URL + url , {headers: {Authorization: 'bearer ' + process.env.REACT_APP_API_TOKEN},});      {/* url is a variable parameter jo ham apne according jaha jo data chahiye uss hisab se denge while ofc base address (localhost.../..) jo env file mai hai uske baad ayega ye*/}
+            console.log(url,'url');
             setData(res.data.data);   // agar res ko console log krenge toh vaha data folder h uske andar ek aur data hai aur fir andar array required milegi; so jb bhi kahi useFetch use krne k baad data.map kr rhe toh => here item == res.data.data
             console.log(res)
           }
